26464c86a1e46aeb4e3cdec620443b033ab72cdd
unknown <razizzahmad21@gmail.com>
2025-01-04 15:09:27 +0700

    Raziz Ahmad Arohmani : Menambahkan foler .vscode yang berisi setting json, lalu menambahkan folder trpc yang berisi react.tsx dan server.ts

---
+++ src/trpc/server.ts
@@ -0,0 +1,15 @@
+import "server-only";
+
+import { cache } from "react";
+import { createCaller } from "~/server/api/root";
+import { createTRPCContext } from "~/server/api/trpc";
+
+/**
+ * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
+ * handling a tRPC call from a React Server Component.
+ */
+const createContext = cache(() => {
+  return createTRPCContext();
+});
+
+export const api = createCaller(createContext);
